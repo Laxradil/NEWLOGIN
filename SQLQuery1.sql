82c815b7dace25f2ec827b3cb5092ddb77923069
Laxradil <brionesfhilip@gmail.com>
2025-05-11 18:52:02 +0800

    Delete SQLQuery1.sql

--- SQLQuery1.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE admin
-(
-        id INT PRIMARY KEY IDENTITY(1,1), 
-        username VARCHAR(MAX) NULL,
-        passowrd VARCHAR(MAX) NULL, 
-        date_created DATE NULL,
-) 
-
-SELECT * FROM admin
